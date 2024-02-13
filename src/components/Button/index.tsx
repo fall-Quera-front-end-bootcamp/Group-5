f6c78868763eb8074c60e4d89ab7c5483a71bed4
Ali-Hedayati <a.hedayati.developer@gmail.com>
2024-02-13 16:23:23 +0330

    First edit for button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+const Button = (porps) => {
+  const handleClick = () => {
+    console.log("hi");
+  };
+  return (
+    <>
+      <div className=" flex items-center justify-center">
+        <button
+          type="button"
+          className="
+       bg-blue-primary
+       text-white-primary
+       w-5/6
+       rounded-md
+       place-content-around
+       font-bold
+       text-bold-xs
+       "
+          onClick={handleClick}
+        >
+          {porps.name}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Button;
+//inline-block rounded px-6 pb-2 pt-2.5 text-xs font-medium leading-normal text-white
