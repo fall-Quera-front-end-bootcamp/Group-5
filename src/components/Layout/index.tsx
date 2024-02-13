5f390509ae71fda41f12cc58c5f352436bb5643a
AMN2080 <alireza3205@gmail.com>
2024-02-14 00:08:06 +0330

    add box for login and register

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,9 @@
 function Layout() {
   return(
   <div className="relative w-full h-screen overflow-hidden flex justify-center items-center">
+    <div className="w-[640px] h-[429px] bg-white rounded-[20px] shadow-[0_30px_60px_-30px_rgba(0,0,0,0.3)] shadow-[0_50px_100px_-20px_(50, 50, 93, 0.25)]">
+
+    </div>
     <div className="absolute top-[450px] w-full h-screen z-[-1] skew-y-[-6deg] bg-gradient-to-l from-[#06846F] to-[#54BEE8]"></div>
   </div>
   )
