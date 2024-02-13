4942ab733d05716c62583d0cc43c9303437340e9
Ali-Hedayati <a.hedayati.developer@gmail.com>
2024-02-13 19:02:53 +0330

    button added to app.tsx for test and a color added to tailwind.config

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-
-import './App.css'
+import Button from "./components/Button";
+import "./App.css";
 
 function App() {
-
   return (
-<>
-<div>
-  <p className='bg-red-primary font-heading text-heading-m text-center'>This is a test page</p>
-</div>
-</>
-  )
+    <>
+      <div>
+        <p className="bg-red-primary font-heading text-heading-m text-center">
+          This is a test page
+        </p>
+        <Button name="Click me" />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
