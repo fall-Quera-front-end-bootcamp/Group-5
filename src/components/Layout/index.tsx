5a640a8453df751eb4061676f389c88a62d806a0
AMN2080 <alireza3205@gmail.com>
2024-02-15 10:58:11 +0330

    add background

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,11 @@
-import Login from "../Authentication/Login"
+// این پایین دوتا کامنت هست که وقتی امیرحسین لاگین رو به اینجا مرج کرد از کامنت در بیار
+
+// import Login from "../Authentication/Login"
 
 function Layout() {
   return(
   <div className="relative w-full h-screen overflow-hidden flex justify-center items-center text-black">
-    <Login/>
+    {/* <Login/> */}
     <div className="absolute top-[450px] w-full h-screen z-[-1] skew-y-[-6deg] bg-gradient-to-l from-[#06846F] to-[#54BEE8]"></div>
   </div>
   )
