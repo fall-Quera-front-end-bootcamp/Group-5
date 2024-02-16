069e3c7fa20bc8ea91d1b4c88fe88cccaac7658e
AMN2080 <alireza3205@gmail.com>
2024-02-16 23:03:39 +0330

    add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 
 import './App.css'
+import Layout from './components/Layout'
 
 function App() {
 
   return (
-<>
-<div>
-  <p className='bg-red-primary font-heading text-heading-m text-center'>This is a test page</p>
-  <h2 className="text-heading-m text-transparent bg-gradient-to-r from-[#118C80] to-[#4AB7D8] bg-clip-text">کوئرا تسک منیجر</h2>
-</div>
-</>
+    <Layout />
   )
 }
 
