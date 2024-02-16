fbc73f3461058c125f2a76af4ec7075bc92e7e3f
AMN2080 <alireza3205@gmail.com>
2024-02-16 22:05:25 +0330

    add header

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,10 @@
-import Login from "../Authentication/Login"
-
 function Layout() {
   return(
   <div className="relative w-full h-screen overflow-hidden flex justify-center items-center text-black">
-    <Login/>
+    <div className="absolute top-[80px] w-10/12 flex justify-between">
+      <p className="font-heading text-heading-l bg-gradient-to-r from-[#118C80] to-[#4AB7D8] text-transparent bg-clip-text">کوئرا تسک منیجر</p>
+    </div>
+    {/* <Login/> */}
     <div className="absolute top-[450px] w-full h-screen z-[-1] skew-y-[-6deg] bg-gradient-to-l from-[#06846F] to-[#54BEE8]"></div>
   </div>
   )
