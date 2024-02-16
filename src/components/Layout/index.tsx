aba853fadf3201ce4cffb533dd05e56524f5f1c7
AMN2080 <alireza3205@gmail.com>
2024-02-16 22:45:19 +0330

    add button

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,6 +3,10 @@ function Layout() {
   <div className="relative w-full h-screen overflow-hidden flex justify-center items-center text-black">
     <div className="absolute top-[80px] w-10/12 flex justify-between">
       <h1 className="font-heading text-heading-l bg-gradient-to-r from-[#118C80] to-[#4AB7D8] text-transparent bg-clip-text">کوئرا تسک منیجر</h1>
+      <div>
+        <p>ثبت‌نام نکرده‌ای؟</p>
+        <button className="h-xl p-[10px] rounded-md bg-brand-primary hover:bg-teal-primary text-white-primary">ثبت‌نام</button>
+      </div>
     </div>
     {/* <Login/> */}
     <div className="absolute top-[450px] w-full h-screen z-[-1] skew-y-[-6deg] bg-gradient-to-l from-[#06846F] to-[#54BEE8]"></div>
