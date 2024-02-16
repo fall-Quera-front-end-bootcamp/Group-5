17bce3e91d96fa6c1b5328d90ef4adfd110e1f85
AMN2080 <alireza3205@gmail.com>
2024-02-16 22:26:17 +0330

    p to h1

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,7 +2,7 @@ function Layout() {
   return(
   <div className="relative w-full h-screen overflow-hidden flex justify-center items-center text-black">
     <div className="absolute top-[80px] w-10/12 flex justify-between">
-      <p className="font-heading text-heading-l bg-gradient-to-r from-[#118C80] to-[#4AB7D8] text-transparent bg-clip-text">کوئرا تسک منیجر</p>
+      <h1 className="font-heading text-heading-l bg-gradient-to-r from-[#118C80] to-[#4AB7D8] text-transparent bg-clip-text">کوئرا تسک منیجر</h1>
     </div>
     {/* <Login/> */}
     <div className="absolute top-[450px] w-full h-screen z-[-1] skew-y-[-6deg] bg-gradient-to-l from-[#06846F] to-[#54BEE8]"></div>
