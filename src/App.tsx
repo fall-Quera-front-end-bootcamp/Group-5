d2f09bff6865fc3838888e885191ee2a2b8c4a51
mitso-ksr <mitsonary@gmail.com>
2024-02-16 22:27:11 +0330

    config edited

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
 <>
 <div>
   <p className='bg-red-primary font-heading text-heading-m text-center'>This is a test page</p>
+  <h2 className="text-heading-m text-transparent bg-gradient-to-r from-[#118C80] to-[#4AB7D8] bg-clip-text">کوئرا تسک منیجر</h2>
 </div>
 </>
   )
