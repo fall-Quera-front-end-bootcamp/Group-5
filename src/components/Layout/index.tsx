81398f8585db9bfc30993ea9097c0f84d7e4089e
amirhossein <79211332+capamir@users.noreply.github.com>
2024-02-18 19:16:52 +0330

    layout style changed

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function Layout() {
   return (
     <div className="relative w-full h-screen overflow-hidden flex justify-center items-center text-black">
-      <div className="absolute top-[80px] w-10/12 flex justify-between">
+      <div className="absolute top-[45px] w-10/12 flex justify-between">
         <h1 className="font-heading text-heading-l bg-gradient-to-r from-[#118C80] to-[#4AB7D8] text-transparent bg-clip-text">
           کوئرا تسک منیجر
         </h1>
