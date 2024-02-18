f66536685789e1fbef5514ff4a3dcda74a98cb12
amirhossein <79211332+capamir@users.noreply.github.com>
2024-02-18 11:48:36 +0330

    React-Router-dom added to project
    file structure changed:
    1- Authentication folder deleted from  component folder
    2- pages/auth folder created
    3- 4 tsx files created for authentication
    4- BrowserRouter added to main.tsx as Router
    5- routes added to App.tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,17 +1,23 @@
+import { Outlet } from "react-router-dom";
+
 function Layout() {
-  return(
-  <div className="relative w-full h-screen overflow-hidden flex justify-center items-center text-black">
-    <div className="absolute top-[80px] w-10/12 flex justify-between">
-      <h1 className="font-heading text-heading-l bg-gradient-to-r from-[#118C80] to-[#4AB7D8] text-transparent bg-clip-text">کوئرا تسک منیجر</h1>
-      <div className="flex justify-center items-center gap-[10px]">
-        <p className="font-body text-bold-m">ثبت‌نام نکرده‌ای؟</p>
-        <button className="w-[95px] h-10 p-2.5 rounded-md duration-300 bg-brand-primary hover:bg-teal-primary text-white font-heading text-body-s select-none">ثبت‌نام</button>
+  return (
+    <div className="relative w-full h-screen overflow-hidden flex justify-center items-center text-black">
+      <div className="absolute top-[80px] w-10/12 flex justify-between">
+        <h1 className="font-heading text-heading-l bg-gradient-to-r from-[#118C80] to-[#4AB7D8] text-transparent bg-clip-text">
+          کوئرا تسک منیجر
+        </h1>
+        <div className="flex justify-center items-center gap-[10px]">
+          <p className="font-body text-bold-m">ثبت‌نام نکرده‌ای؟</p>
+          <button className="w-[95px] h-10 p-2.5 rounded-md duration-300 bg-brand-primary hover:bg-teal-primary text-white font-heading text-body-s select-none">
+            ثبت‌نام
+          </button>
+        </div>
       </div>
+      <Outlet />
+      <div className="absolute top-[450px] w-full h-screen z-[-1] skew-y-[-6deg] bg-gradient-to-l from-[#06846F] to-[#54BEE8]"></div>
     </div>
-    {/* <Login/> */}
-    <div className="absolute top-[450px] w-full h-screen z-[-1] skew-y-[-6deg] bg-gradient-to-l from-[#06846F] to-[#54BEE8]"></div>
-  </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
