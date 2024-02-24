4c54850e4b648379b29ef4f3eca056973f603868
amirhossein <79211332+capamir@users.noreply.github.com>
2024-02-24 16:04:42 +0330

    implimenting secondary colors to colors' list

--- src/services/Theme.ts
+++ src/services/Theme.ts
@@ -10,11 +10,19 @@ const colors: ColorsType = [
       bgPrimary: "bg-red-primary",
       textPrimary: "text-red-primary",
     },
+    secondary: {
+      bgSecondary: "bg-red-secondary",
+      textSecondary: "text-red-secondary",
+    },
   },
   {
     primary: {
       bgPrimary: "bg-pink-primary",
       textPrimary: "text-pink-primary",
+    },
+    secondary: {
+      bgSecondary: "bg-pink-secondary",
+      textSecondary: "text-pink-secondary",
     },
   },
   {
@@ -22,11 +30,19 @@ const colors: ColorsType = [
       bgPrimary: "bg-grape-primary",
       textPrimary: "text-grape-primary",
     },
+    secondary: {
+      bgSecondary: "bg-grape-secondary",
+      textSecondary: "text-grape-secondary",
+    },
   },
   {
     primary: {
       bgPrimary: "bg-violet-primary",
       textPrimary: "text-violet-primary",
+    },
+    secondary: {
+      bgSecondary: "bg-violet-secondary",
+      textSecondary: "text-violet-secondary",
     },
   },
   {
@@ -34,11 +50,19 @@ const colors: ColorsType = [
       bgPrimary: "bg-indigo-primary",
       textPrimary: "text-indigo-primary",
     },
+    secondary: {
+      bgSecondary: "bg-indigo-secondary",
+      textSecondary: "text-indigo-secondary",
+    },
   },
   {
     primary: {
       bgPrimary: "bg-blue-primary",
       textPrimary: "text-blue-primary",
+    },
+    secondary: {
+      bgSecondary: "bg-blue-secondary",
+      textSecondary: "text-blue-secondary",
     },
   },
   {
@@ -46,11 +70,19 @@ const colors: ColorsType = [
       bgPrimary: "bg-cyan-primary",
       textPrimary: "text-cyan-primary",
     },
+    secondary: {
+      bgSecondary: "bg-cyan-secondary",
+      textSecondary: "text-cyan-secondary",
+    },
   },
   {
     primary: {
       bgPrimary: "bg-teal-primary",
       textPrimary: "text-teal-primary",
+    },
+    secondary: {
+      bgSecondary: "bg-teal-secondary",
+      textSecondary: "text-teal-secondary",
     },
   },
   {
@@ -58,11 +90,19 @@ const colors: ColorsType = [
       bgPrimary: "bg-brand-primary",
       textPrimary: "text-brand-primary",
     },
+    secondary: {
+      bgSecondary: "bg-brand-secondary",
+      textSecondary: "text-brand-secondary",
+    },
   },
   {
     primary: {
       bgPrimary: "bg-green-primary",
       textPrimary: "text-green-primary",
+    },
+    secondary: {
+      bgSecondary: "bg-green-secondary",
+      textSecondary: "text-green-secondary",
     },
   },
   {
@@ -70,17 +110,29 @@ const colors: ColorsType = [
       bgPrimary: "bg-lime-primary",
       textPrimary: "text-lime-primary",
     },
+    secondary: {
+      bgSecondary: "bg-lime-secondary",
+      textSecondary: "text-lime-secondary",
+    },
   },
   {
     primary: {
       bgPrimary: "bg-yellow-primary",
       textPrimary: "text-yellow-primary",
     },
+    secondary: {
+      bgSecondary: "bg-yellow-secondary",
+      textSecondary: "text-yellow-secondary",
+    },
   },
   {
     primary: {
       bgPrimary: "bg-orange-primary",
       textPrimary: "text-orange-primary",
+    },
+    secondary: {
+      bgSecondary: "bg-orange-secondary",
+      textSecondary: "text-orange-secondary",
     },
   },
 ];
