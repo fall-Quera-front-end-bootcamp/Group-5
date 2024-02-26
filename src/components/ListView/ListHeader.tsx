718f0ff40e143b46186442c15f43113ab12791a7
Ali-Hedayati <a.hedayati.developer@gmail.com>
2024-02-26 21:32:46 +0330

    Refactor codes

---
+++ src/components/ListView/ListHeader.tsx
@@ -0,0 +1,37 @@
+import { IoIosArrowDropdown } from "react-icons/io";
+import { TaskStatusEnum } from "../Task/TaskStatusEnum";
+
+export type taskStatusType = "done" | "pending" | "inprogress";
+// export type taskStatusType = string;
+
+type ListHeaderType = {
+  status: taskStatusType;
+  onToggleCollapseStatus: () => void;
+};
+
+export const ListHeader: React.FC<ListHeaderType> = ({
+  status,
+  onToggleCollapseStatus,
+}) => {
+  return (
+    <div className="flex flex-row">
+      <div className="basis-[50%] flex " onClick={onToggleCollapseStatus}>
+        <IoIosArrowDropdown className="mt-2.5 ml-2 size-5" />
+        <div
+          key={status}
+          className={`${TaskStatusEnum[status].color} text-center
+                 text-white 
+                 min-w-min rounded-lg 
+                 font-heading-l 
+                 p-2 `}
+        >
+          {TaskStatusEnum[status].textEn}
+        </div>
+      </div>
+      <span className="basis-[15%] text-center">اعضا</span>
+      <span className="basis-[15%] text-center">ددلاین</span>
+      <span className="basis-[15%] text-center">اولویت</span>
+      <span className="basis-[15%] text-center">توضیحات</span>
+    </div>
+  );
+};
