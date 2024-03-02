5b04279287bdcc85627daba0f4eb7a982e7f8052
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-02 20:36:04 +0330

    use react-dropzone to upload image

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -3,6 +3,14 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import FieldType from "../entities/Field";
 
+const MAX_FILE_SIZE = 5000000;
+const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
 interface FormsType {
   [key: string]: {
     title: string;
@@ -19,6 +27,13 @@ const forms: FormsType = {
     label: "ثبت تغییرات",
     button: "personal",
     schema: {
+      image: z
+        .any()
+        .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+        .refine(
+          (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+          "Only .jpg, .jpeg, .png and .webp formats are supported."
+        ),
       firstname: z
         .string()
         .min(3, { message: "نام باید حداقل 3 کاراکتر باشد." }),
@@ -98,16 +113,15 @@ const useProfile = (tab: string) => {
   } = useForm({
     resolver:
       button === "account"
-        ? schema &&
-          zodResolver(
+        ? zodResolver(
             z
-              .object(schema)
+              .object(schema!)
               .refine((data) => data.password === data.confirmPassword, {
                 path: ["confirmPassword"],
                 message: "پسورد و تکرار آن باید یکسان باشد.",
               })
           )
-        : schema && zodResolver(z.object(schema)),
+        : zodResolver(z.object(schema!)),
   });
 
   return { title, fields, button, label, register, handleSubmit, errors };
