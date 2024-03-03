906caf8f6b131cc1f777c814dcf17459e0293cc7
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-03 19:38:00 +0330

    error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import Layout from "./components/Layout";
-import { Login, SignUp, ForgotPassword, ResetPassword, Profile } from "./Pages";
+import { Login, SignUp, ForgotPassword, ResetPassword, Profile } from "./pages";
 import { Routes, Route } from "react-router-dom";
-import Listview from "./components/ListView/Listview";
 import MainLayout from "./components/MainLayout";
 
 function App() {
@@ -13,11 +12,10 @@ function App() {
         <Route path="signUp" element={<SignUp />} />
         <Route path="forgotPassword" element={<ForgotPassword />} />
         <Route path="resetPassword" element={<ResetPassword />} />
-        <Route path="Listview" element={<Listview />} />
       </Route>
       <Route path="/profile" element={<Profile />} />
-      <Route path="/home" element={<MainLayout />} > 
-        <Route index element = {<h1>This will be rendered</h1>} />
+      <Route path="/home" element={<MainLayout />}>
+        <Route index element={<h1>This will be rendered</h1>} />
       </Route>
     </Routes>
   );
