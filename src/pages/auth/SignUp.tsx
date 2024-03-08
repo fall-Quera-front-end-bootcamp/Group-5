de12eb0b7329ffee6cc799b19db2f4d760677fcf
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-08 13:58:37 +0330

    basic handle submit function to sign up

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,5 +1,11 @@
 import { Form } from "../../components";
+import { ValueType } from "../../entities/Auth";
 
-const signUp = () => <Form formClass={"signUp"} />;
+const signUp = () => {
+  const onSubmit = (data: ValueType) => {
+    console.log(data);
+  };
+  return <Form formClass={"signUp"} onSubmit={onSubmit} />;
+};
 
 export default signUp;
