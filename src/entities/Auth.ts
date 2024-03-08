61ab2066994a58b9e7c7cb10fd592150a969e8e5
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-08 13:58:04 +0330

    authentication's types

--- src/entities/Auth.ts
+++ src/entities/Auth.ts
@@ -1,3 +1,7 @@
+export type ValueType = {
+  [key: string]: string | string[] | ValueType;
+};
+
 export default interface FormsType {
   [key: string]: {
     title: string;
@@ -6,4 +10,34 @@ export default interface FormsType {
     schema: Record<string, any>;
     fields: { key: string; type: string; label: string }[];
   };
+}
+
+export interface LoginFormData {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  id: number;
+  email: string;
+  username: string;
+  tokens: {
+    access: string;
+    refresh: string;
+  };
+}
+
+export interface SignupFormData {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface RefreshTokenData {
+  refresh: string;
+}
+
+export interface RefreshTokenResponse {
+  access: string;
+  refresh: string;
 }
