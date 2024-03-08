c3863db09efe4196c496fd3e52e56f6bb432ba8a
Alireza Mohamadzade <alireza3205@gmail.com>
2024-03-08 22:15:35 +0330

    icon & hover animation in RenderCells

--- src/components/BoardCalendarView/RenderCells.tsx
+++ src/components/BoardCalendarView/RenderCells.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FaPlusSquare } from "react-icons/fa";
+
 export type DayType = {
   dayIndex: number;
 };
@@ -12,19 +15,23 @@ const DaysEnum: { [key: number]: string } = {
   6: "جمعه",
 };
 
-const RenderCells: React.FC<DayType> = ({
-  dayIndex,
-}): JSX.Element => {
+const RenderCells: React.FC<DayType> = ({ dayIndex }): JSX.Element => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="border-l-[1px] border-b-[1px] border-black relative">
-      <div
-        className="w-40 max-h-[133px] p-2"
-      >
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="relative border-l-[1px] border-b-[1px] border-black transition duration-500 ease-in-out"
+    >
+      <div className="w-40 max-h-[133px] p-2">
         {dayIndex < 7 && <div className="absolute right-1 top-1">{DaysEnum[dayIndex]}</div>}
         <div className="absolute bottom-1 left-2">{dayIndex}</div>
+        <div className="text-brand-primary cursor-pointer absolute bottom-2 right-2">
+          <FaPlusSquare size={20} style={{ opacity: isHovered ? 1 : 0, transition: "opacity 0.5s" }} />
+        </div>
       </div>
     </div>
   );
 };
 
-export default RenderCells
+export default RenderCells;
