032f4f4177efb949a7fc5bff52f0326a321c7667
Ali-Hedayati <a.hedayati.developer@gmail.com>
2024-03-10 13:09:22 +0330

    PermissionList component was added to ShareProject modal

--- src/components/common/Modals/PermissionList.tsx
+++ src/components/common/Modals/PermissionList.tsx
@@ -1,17 +1,32 @@
-type PropsType = {
-  setShowModal: (showModal: boolean) => void
-}
-
-const PermissionList = ({setShowModal}:PropsType) => {
+export  const PermissionList: React.FC = () => {
   return (
     <>
-      <div
-        className="absolute top-[-10px] left-[-260px] w-64 h-96 mt-1 text-body-s font-normal rounded-lg p-3 z-20 bg-red-primary shadow-lg"
-      >
-        ممد نوبری دل همه رو میبری
+      <div className="flex flex-col gap-7">
+        <div>
+          <h3 className="pb-2 font-bold ">دسترسی کامل</h3>
+          <p className="text-[#7D828C]">
+            توانایی ساختن تسک در این پروژه، ویرایش تنظیمات پروژه و حذف پروژه
+          </p>
+        </div>
+        <div>
+          <h3 className="pb-2 font-bold ">دسترسی ویرایش</h3>
+          <p className="text-[#7D828C]">
+            توانایی ویرایش تسک در این پروژه و ویرایش تنظیمات پروژه. نمی‌تواند
+            پروژه را حذف یا تسک جدید بسازد.
+          </p>
+        </div>
+        <div>
+          <h3 className="pb-2 font-bold ">دسترسی کامنت</h3>
+          <p className="text-[#7D828C]">
+            توانایی کامنت گذاشتن دارد. می‌تواند ستون تسک‌ها را تغییر دهد اما
+            توانایی ویرایش تنظیمات پروژه را ندارد.
+          </p>
+        </div>
+        <div>
+          <h3 className="pb-2 font-bold ">فقط دسترسی مشاهده</h3>
+          <p className="text-[#7D828C]">توانایی گذاشتن کامنت یا ویرایش تسک‌ها را ندارد.</p>
+        </div>
       </div>
     </>
   );
 };
-
-export default PermissionList;
--- src/components/common/Modals/ShareProject.tsx
+++ src/components/common/Modals/ShareProject.tsx
@@ -1,18 +1,17 @@
-import { useState } from "react";
-import PermissionList from "./PermissionList" 
-import useLockBodyScroll from "../../../hooks/useLockBodyScroll"
+import { PermissionList } from "./PermissionList";
+import useLockBodyScroll from "../../../hooks/useLockBodyScroll";
 import { IoClose } from "react-icons/io5";
 import { FaLink } from "react-icons/fa6";
+import Popup from "reactjs-popup";
+
 import { IoIosArrowDown } from "react-icons/io";
 
 type PropsType = {
-  setShowModal: (showModal: boolean) => void
-}
+  setShowModal: (showModal: boolean) => void;
+};
 
-const ShareProject = ({setShowModal}:PropsType) => {
+const ShareProject = ({ setShowModal }: PropsType) => {
   useLockBodyScroll();
-
-  const [showPermission, setShowPermission] = useState(false);
 
   return (
     <>
@@ -25,7 +24,9 @@ const ShareProject = ({setShowModal}:PropsType) => {
             >
               <IoClose />
             </button>
-            <h2 className="text-center font-heading text-heading-xs">به اشتراک‌گذاری پروژه</h2>
+            <h2 className="text-center font-heading text-heading-xs">
+              به اشتراک‌گذاری پروژه
+            </h2>
             <span className="w-6" />
           </div>
           <div className="flex flex-col gap-6">
@@ -35,14 +36,18 @@ const ShareProject = ({setShowModal}:PropsType) => {
                 placeholder="دعوت با ایمیل"
                 type="email"
               />
-              <button className="w-1/5 h-full transition-all bg-brand-primary hover:bg-teal-primary text-white">ارسال</button>
+              <button className="w-1/5 h-full transition-all bg-brand-primary hover:bg-teal-primary text-white">
+                ارسال
+              </button>
             </div>
             <div className="flex justify-between text-sm">
               <div className="flex items-center gap-2">
                 <FaLink />
                 <span>لینک خصوصی</span>
               </div>
-              <button className="hover:bg-gray-100 text-xs px-3 py-1 border rounded-md">کپی لینک</button>
+              <button className="hover:bg-gray-100 text-xs px-3 py-1 border rounded-md">
+                کپی لینک
+              </button>
             </div>
             <div className="flex flex-col gap-4">
               <p className="text-[#7D828C] text-sm">اشتراک‌گذاشته شده با</p>
@@ -50,12 +55,20 @@ const ShareProject = ({setShowModal}:PropsType) => {
                 <div className="flex justify-between items-center">
                   <div className="flex items-center object-cover object-center gap-3">
                     <div className="flex items-center gap-2">
-                      <img className="rounded-full w-9 h-9" src="https://s3-alpha-sig.figma.com/img/0e35/b8e1/c386fc03b862ed171fd63bb7292d7d01?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Iear2vlOY0kMXWnxMDhcdhd4~VIxLgBI~~wG1XLsPfC78bhDGxsjU1-qKWmuKZ2dKV0TOfoUkCUN-mGZAFyIDc0UNwvSJ1JHf4BtArGvIR1ILiH1jU~LhKTvVfKB6jKELi4eEpD1F4Y2BEkeahyJkOA-I~NzAQloznhh1br1bsq3Y0fxtRbr7pZp0QHpqFRI6EFliOsMhEmJnmt1WGe~AxChOyGmrjwTVYb6fAFhUTfmYIGE6IBuv6u4cvsw3D8zvE2OlAqi7ndG4r~GAi14u2tQc1qvymDJSm2CY~hk~WcSefvmCn-cJdEujfpSIuGl-sh~aVNIjKnIjILUyfECPw__" alt="" />
+                      <img
+                        className="rounded-full w-9 h-9"
+                        src="https://s3-alpha-sig.figma.com/img/0e35/b8e1/c386fc03b862ed171fd63bb7292d7d01?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Iear2vlOY0kMXWnxMDhcdhd4~VIxLgBI~~wG1XLsPfC78bhDGxsjU1-qKWmuKZ2dKV0TOfoUkCUN-mGZAFyIDc0UNwvSJ1JHf4BtArGvIR1ILiH1jU~LhKTvVfKB6jKELi4eEpD1F4Y2BEkeahyJkOA-I~NzAQloznhh1br1bsq3Y0fxtRbr7pZp0QHpqFRI6EFliOsMhEmJnmt1WGe~AxChOyGmrjwTVYb6fAFhUTfmYIGE6IBuv6u4cvsw3D8zvE2OlAqi7ndG4r~GAi14u2tQc1qvymDJSm2CY~hk~WcSefvmCn-cJdEujfpSIuGl-sh~aVNIjKnIjILUyfECPw__"
+                        alt=""
+                      />
                       <span>من</span>
                     </div>
-                    <div className="flex justify-center items-center h-6 px-2 py-1 bg-blue-secondary text-xs text-[#228BE6] rounded-md">مالک ورک‌اسپیس</div>
+                    <div className="flex justify-center items-center h-6 px-2 py-1 bg-blue-secondary text-xs text-[#228BE6] rounded-md">
+                      مالک ورک‌اسپیس
+                    </div>
                   </div>
-                  <button className="h-6 hover:bg-gray-100 text-xs px-3 py-1 border rounded-md">دسترسی کامل</button>
+                  <button className="h-6 hover:bg-gray-100 text-xs px-3 py-1 border rounded-md">
+                    دسترسی کامل
+                  </button>
                 </div>
                 <div className="relative flex justify-between items-center">
                   <div className="flex items-center object-cover object-center gap-3">
@@ -69,14 +82,39 @@ const ShareProject = ({setShowModal}:PropsType) => {
                       <span>alireza3205@gmail.com</span>
                     </div>
                   </div>
-                  <button
+                  <Popup
+                    trigger={
+                      <div
+                        className="h-6 flex items-center
+                        gap-2 hover:bg-gray-100
+                        text-xs px-3 py-1 border
+                        rounded-md
+                        "
+                      >
+                        <span>دسترسی کامل</span>
+                        <IoIosArrowDown />
+                      </div>
+                    }
+                    contentStyle={{
+                      width: "300px",
+                      margin: "auto",
+                      background: "rgb(255, 255, 255)",
+                      padding: "25px",
+                    }}
+                    position="left center"
+                  >
+                    <PermissionList />
+                  </Popup>
+                  {/* <button
                     className="h-6 flex items-center gap-2 hover:bg-gray-100 text-xs px-3 py-1 border rounded-md"
                     onClick={() => setShowPermission(true)}
                   >
                     <span>دسترسی کامل</span>
                     <IoIosArrowDown />
-                  </button>
-                  {showPermission && <PermissionList setShowModal={setShowPermission} />}
+                  </button> */}
+                  {/* {showPermission && (
+                    <PermissionList setShowModal={setShowPermission} />
+                  )} */}
                 </div>
               </div>
             </div>
