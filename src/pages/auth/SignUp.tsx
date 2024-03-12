df02e36f16a814a716f37ba9b1a9621870951093
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-12 16:13:50 +0330

    resolving f*cking problems :)

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,15 +1,3 @@
-import { Form } from "../../components";
-import { ValueType } from "../../entities/Auth";
-
-const signUp = () => {
-  const onSubmit = (data: ValueType) => {
-    console.log(data);
-  };
-  return <Form formClass={"signUp"} onSubmit={onSubmit} />;
-};
-
-export default signUp;
-
 import Form from "../../components/common/Form/index";
 import { LoginResponse } from "../../entities/Auth";
 import { useRegisterUser } from "../../hooks/useAuth";
