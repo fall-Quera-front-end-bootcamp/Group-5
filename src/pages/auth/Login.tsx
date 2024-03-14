f68f7d4a9478d8e14682cb863ecba2e9337c5c69
amirhossein <79211332+capamir@users.noreply.github.com>
2024-03-14 19:40:35 +0330

    add navigate to login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,11 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import Form from "../../components/common/Form/index";
 import { LoginResponse } from "../../entities/Auth";
 import { useLoginUser } from "../../hooks/useAuth";
 
 const Login = () => {
   const login = useLoginUser();
+  const navigate = useNavigate();
   const onSubmit = (data: LoginResponse) => {
     login.mutate(data);
+    navigate("/");
   };
   return (
     <Form formClass={"signIn"} onSubmit={onSubmit} mutateError={login.error} />
