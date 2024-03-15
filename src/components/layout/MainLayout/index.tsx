2473eff154b49c06e2cb8caf22034a4f3126b14e
mitso-ksr <mitsonary@gmail.com>
2024-03-15 21:48:36 +0330

    button added

--- src/components/layout/MainLayout/index.tsx
+++ src/components/layout/MainLayout/index.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../Sidebar/MySideBar";
 import SidebarHeader from "../Sidebar/SidebarHeader";
+import { VscDiffAdded } from "react-icons/vsc";
 
 interface MainLayoutProps {
   Displayheader: boolean;
@@ -8,13 +9,19 @@ interface MainLayoutProps {
 
 const MainLayout = ({ Displayheader }: MainLayoutProps) => {
   return (
-    <div className="flex gap-s">
+    <div className="flex relative gap-s">
       <Sidebar className="h-12" />
       <div className="flex flex-wrap w-full mt-10 gap-5">
         <div className="w-full">{Displayheader && <SidebarHeader />}</div>
         <div className="w-full pb-5">
           <Outlet />
         </div>
+      </div>
+      <div className="absolute top-[850px] left-[50px] w-[111px] h-[40px] bg-brand-primary rounded-md ">
+        <button className="h-full w-ful flex items-center justify-center gap-[4px] ml-auto mr-auto">
+        <VscDiffAdded className="text-white h-[20px] w-[20px]"/>
+        <p className="text-body-s font-body text-white"> تسک جدید</p>
+        </button>
       </div>
     </div>
   );
