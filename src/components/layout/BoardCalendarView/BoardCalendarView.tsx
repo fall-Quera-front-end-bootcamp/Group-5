503e2a8d771336475e1a55e1a96d08282690cc74
mitso-ksr <mitsonary@gmail.com>
2024-03-15 23:12:37 +0330

    calenderView Added

--- src/components/layout/BoardCalendarView/BoardCalendarView.tsx
+++ src/components/layout/BoardCalendarView/BoardCalendarView.tsx
@@ -4,7 +4,7 @@ const BoardCalendarView: React.FC = () => {
   return (
     <div className="w-full p-10">
       <div className="w-full h-full border-r-[1px] border-t-[1px] border-black z-10">
-        <div className="w-full h-screen grid grid-cols-7">
+        <div className="w-full h-[80vh] grid grid-cols-7">
           {Array.from({ length: 35 }).map((_, index) =>
             <RenderCells dayIndex={index} key={index} />
           )}
